777377d9bfa6b2203bd2bb565499a1374d969103
DocFromSwamps <romkadin@gmail.com>
2025-04-23 10:05:01 +0300

    script_cms.sh

--- script_cms.sh
+++ script_cms.sh
@@ -1,19 +1,19 @@
-apt-get update
+pt-get update
 apt-get install -y apache2
 apt-get install -y php8.1
 apt-get remove -y php8.0
-apt-get install -y php8.1-mysqli php8.1-xml php8.1-intl php8.1-zip php8.1-curl php8.1-mbstring php8.1-soap php8.1-ldap php8.1-bcmath 
+apt-get install -y php8.1-mysqli php8.1-xml php8.1-intl php8.1-zip php8.1-curl php8.1-mbstring php8.1-soap php8.1-ldap php8.1-bcmath
 apt-get install -y apache2-mod_php8.1
 systemctl enable –now httpd2
 systemctl start mariadb
 mysql_secure_installation
 mysql -u root -p
-create database moodledb character set utf8mb4 collate utf8mb4_unicode_ci;
+create database moodledb character nset utf8mb4 collate utf8mb4_unicode_ci;
 create user ‘moodle’@’localhost’ identified by ‘password’;
 grant all privileges on moodledb.* to ‘moodle’@’localhost’;
 flush privileges;
 exit;
-git clone git://git.moodle.org/moodle.git (если выскакивает ошибка попробуйте еще несколько раз, должно заработать)
+git clone git://git.moodle.org/moodle.git
 cd moodle
 git branch -a
 git branch --track MOODLE_405_STABLE origin/MOODLE_405_STABLE
