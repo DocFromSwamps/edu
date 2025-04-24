8c34a99b876a8d4118626ba28bc925c4c03f7678
DocFromSwamps <romkadin@gmail.com>
2025-04-24 11:44:55 +0300

    script_cms_2.sh

---
+++ script_cms_2.sh
@@ -0,0 +1,16 @@
+git clone git://git.moodle.org/moodle.git
+cd moodle
+git branch --track MOODLE_405_STABLE origin/MOODLE_405_STABLE
+git checkout MOODLE_405_STABLE
+cd
+cp -R moodle /var/www/html
+mkdir /var/moodledata
+chmod 777 /var/moodledata
+chown -R apache2:apache2 /var/www/html/moodle
+chmod -R 755 /var/www/html/moodle
+rm -rf /etc/httpd2/conf/sites-available/moodle.conf
+cp /home/git/edu/moodle.conf /etc/httpd2/conf/sites-available/
+ln -s /etc/httpd2/conf/sites-available/moodle.conf /etc/httpd2/conf/sites-enabled/
+apachectl configtest
+echo "max_input_vars = 5000" >> /etc/php/8.1/apache2-mod_php/php.ini
+systemctl restart httpd2
