d4349755d82af87e6c187b47f651a72c5750ba3f
DocFromSwamps <romkadin@gmail.com>
2025-04-24 11:15:02 +0300

    script_transport_br.sh

--- script_transport_br.sh
+++ script_transport_br.sh
@@ -1,5 +1,5 @@
 apt-get install nftables -y
-systemctl enable --now nftables  
+systemctl enable --now nftables
 rm -rf /etc/nftables/nftables.nft
-cp /home/git/education/nftables_br.nft /etc/nftables/nftables.nft
+cp /home/git/edu/nftables_br/nft /etc/nftables/nftables.nft
 systemctl restart nftables
