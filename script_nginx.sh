858aa0553006880a97c3239286ac8108ed7809bd
DocFromSwamps <romkadin@gmail.com>
2025-04-24 10:56:52 +0300

    Add files via upload

--- script_nginx.sh
+++ script_nginx.sh
@@ -1,6 +1,6 @@
- apt-get install nginx -y
-  systemctl enable --now nginx
-  systemctl stop ahttpd 
-  rm -rf /etc/nginx/nginx.conf 
-  cp /home/git/education/nginx.conf /etc/nginx/nginx.conf
-  systemctl restart nginx
+apt-get install nginx -y
+systemctl enable --now nginx
+systemctl stop ahttpd
+rm -rf /etc/nginx/nginx.conf
+cp /home/git/edu/nginx.conf /etc/nginx/nginx.conf
+systemctl restart nginx
