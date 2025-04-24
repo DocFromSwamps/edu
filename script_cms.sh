a200fcfee024f0bf3104ccd500cf4a992640415c
DocFromSwamps <romkadin@gmail.com>
2025-04-24 11:12:21 +0300

    script_cms.sh

--- script_cms.sh
+++ script_cms.sh
@@ -8,11 +8,13 @@ systemctl enable --now httpd2
 systemctl start mariadb
 mysql_secure_installation
 mysql -u root -p
+
 create database moodledb character set utf8mb4 collate utf8mb4_unicode_ci;
 create user 'moodle'@'localhost' identified by 'password';
 grant all privileges on moodledb.* to 'moodle'@'localhost';
 flush privileges;
 exit;
+
 git clone git://git.moodle.org/moodle.git
 cd moodle
 git branch -a
