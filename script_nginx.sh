314e4edc038c3b357762789ba3c253458af89b44
DocFromSwamps <romkadin@gmail.com>
2025-04-24 11:46:08 +0300

    script_nginx.sh

--- script_nginx.sh
+++ script_nginx.sh
@@ -1,6 +1,7 @@
 apt-get install nginx -y
 systemctl enable --now nginx
 systemctl stop ahttpd
+systemctl stop httpd2
 rm -rf /etc/nginx/nginx.conf
 cp /home/git/edu/nginx.conf /etc/nginx/nginx.conf
 systemctl restart nginx
