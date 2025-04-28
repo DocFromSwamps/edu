1b1fbeb0dee49b08a76f3aacf1eeb98489891575
DocFromSwamps <romkadin@gmail.com>
2025-04-28 11:24:15 +0300

    script_cms_2.sh

--- script_cms_2.sh
+++ script_cms_2.sh
@@ -8,7 +8,9 @@ mkdir /var/moodledata
 chmod 777 /var/moodledata
 chown -R apache2:apache2 /var/www/html/moodle
 chmod -R 755 /var/www/html/moodle
-rm -rf /etc/httpd2/conf/sites-available/moodle.conf /etc/httpd2/conf/sites-enabled/
+rm -rf /etc/httpd2/conf/sites-available/moodle.conf
+cp /home/git/edu/moodle.conf /etc/httpd2/conf/sites-available/moodle.conf
+ln -s /etc/httpd2/conf/sites-available/moodle.conf /etc/httpd2/conf/sites-enabled/
 apachectl configtest
 echo "max_inputs_vars = 5000" >> /etc/php/8.1/apache2-mod_php/php.ini
 systemctl restart httpd2
