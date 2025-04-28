bcf1d91640eb0a9a61d297b8a80c517c635d2f20
DocFromSwamps <romkadin@gmail.com>
2025-04-28 09:47:31 +0300

    script_samba.sh

--- script_samba.sh
+++ script_samba.sh
@@ -14,7 +14,7 @@ samba-tool group addmembers "Allowed RODC Password Replication Group" hq
 realm join -U user1.hq au-team.irpo
 realm join -U user2.hq au-team.irpo
 realm permit -g hq
-echo "%hq ALL=(ALL) NOPASSWD:/bin/cat,/bin/grep,/bin/id" > /etc/sudoers.d/hq
+echo "%hq ALL=(ALL) NOPASSWD:/bin/cat,/bin/grep,/bin/id" >> /etc/sudoers.d/hq
 rm -rf /opt/import_users.sh
 cp /home/git/edu/import_users.sh /opt/import_users.sh
 chmod +x /opt/import_users.sh
