ea9bec1296c4d52fb684fe8cf35c44e2d317a362
DocFromSwamps <romkadin@gmail.com>
2025-04-28 14:13:11 +0300

    script_cms.sh

--- script_cms.sh
+++ script_cms.sh
@@ -1,9 +1,10 @@
 apt-get update
 apt-get install -y apache2
-apt-get install -y php8.1
+apt-get install -y php8.2
+apt-get remove -y php8.1
 apt-get remove -y php8.0
-apt-get install -y php8.1-mysqli php8.1-xml php8.1-intl php8.1-zip php8.1-curl php8.1-mbstring php8.1-soap php8.1-ldap php8.1-bcmath
-apt-get install -y apache2-mod_php8.1
+apt-get install -y php8.2-mysqli php8.2-xml php8.2-intl php8.2-zip php8.2-curl php8.2-mbstring php8.2-soap php8.2-ldap php8.2-bcmath
+apt-get install -y apache2-mod_php8.2
 systemctl enable --now httpd2
 systemctl start mariadb
 mysql_secure_installation
