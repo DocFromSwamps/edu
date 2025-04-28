0a671ff991441425b63b55c3dc9ccb47605cde49
DocFromSwamps <romkadin@gmail.com>
2025-04-28 11:10:53 +0300

    Add files via upload

--- script_transport_hq.sh
+++ script_transport_hq.sh
@@ -1,5 +1,5 @@
-apt-get install nftables -y
+apt-get install nftables  -y
 systemctl enable --now nftables
 rm -rf /etc/nftables/nftables.nft
-cp /home/git/edu/nftables_hq.nft /etc/nftables/nftables.nft
+cp /home/git/edu/nftables_hq.nft /nftables/nftables.nft
 systemctl restart nftables
